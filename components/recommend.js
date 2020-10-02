cb3a2a5e7aafbeda1c3efa8765c4e65391f7f93c
DongjaeJang <jangdj001234@gmail.com>
2020-10-02 15:35:53 +0900

    modify Recommend View

--- components/recommend.js
+++ components/recommend.js
@@ -1,32 +1,83 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
-import Recommend_result from "./recommend_result";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Dimensions,
+  Image,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const screenWidth = Math.round(Dimensions.get("window").width);
+const screenHeight = Math.round(Dimensions.get("window").height);
 
 const Recommend = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Button
-        onPress={() => navigation.navigate("Recommend_result")}
-        title="recommend"
-      />
+      <View style={styles.imageLine}>
+        <Image source={require("../images/book_icon.png")} />
+        <Text style={styles.header}> FolioChain</Text>
+      </View>
+      <View style={{ alignItems: "center", paddingBottom: 50 }}>
+        <Text style={styles._header}>안녕하세요 !</Text>
+        <Image source={require("../images/AI.png")} />
+      </View>
+      <View>
+        <TouchableOpacity
+          style={{ flexDirection: "row", paddingLeft: 50 }}
+          onPress={() => alert("관심기업")}
+        >
+          <Ionicons name="md-search" size={50} color="#f1c40f" />
+          <Text style={styles.text}> 관심 기업 검색하기</Text>
+        </TouchableOpacity>
+        <View>
+          <Text style={{ paddingBottom: 30 }}></Text>
+        </View>
+        <TouchableOpacity
+          style={{ flexDirection: "row", paddingLeft: 50 }}
+          onPress={() => alert("챗봇")}
+        >
+          <Ionicons name="md-chatbubbles" size={50} color="#f1c40f" />
+          <Text style={styles.text}> Chat bot</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  header: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#025880",
-  },
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    width: screenWidth,
+    height: screenHeight,
+    backgroundColor: "#112f4c",
+  },
+  imageLine: {
+    paddingLeft: 10,
+    flexDirection: "row",
+    paddingTop: 30,
+    paddingBottom: 50,
+  },
+  header: {
+    fontSize: 30,
+    paddingTop: 5,
+    fontWeight: "bold",
+    color: "white",
+  },
+  _header: {
+    fontSize: 30,
+    paddingTop: 5,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    paddingBottom: 20,
   },
   text: {
-    fontSize: 20,
+    fontSize: 30,
+    paddingTop: 5,
     fontWeight: "bold",
+    color: "white",
+    paddingLeft: 15,
   },
 });
 
